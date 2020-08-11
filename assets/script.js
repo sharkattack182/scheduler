f78f0f71ceadb3c6717b77edd5dccc2b1e181418
Matthew Courtney <matthewdc182@gmail.com>
2020-08-11 16:20:42 -0400

    added the time to the jumbotron and added the script tag to html

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//psudo
+//1 display time
+//2 adjust color of div relative to time
+//3 store text entry to local storage
+
+
+//display time
+
+$("#currentDay").text(moment().format( "MMMM Do YYYY"));
