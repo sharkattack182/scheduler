e227de6971b31d7955d4b5c559f459ecf7aa93f4
Matthew Courtney <matthewdc182@gmail.com>
2020-08-11 20:23:23 -0400

    added in the code to change the current hour of the day to the css stylings for the class present

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,18 @@
 //psudo
 //1 display time
 //2 adjust color of div relative to time
+//2a chaeck to see if the current time is equal to the hour in the callander
 //3 store text entry to local storage
 
 
 //display time
 
 $("#currentDay").text(moment().format( "MMMM Do YYYY"));
+
+var currentTime = moment().format("HH");  //set variable for military time
+for (var i = 9; i < 18; i++) {      // i will run a loop for numbers 9-18 
+    if(i === parseInt(currentTime)) {   //this line compares the number generated above to the current hour in military time
+        $("#" + i).addClass("present");         // using jq i added a class to the div with the id whos number matches the correct time above
+    }
+}
+
